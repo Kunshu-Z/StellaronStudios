2a1dff5818b9113dab356e58f4285b81ecc5b0d9
Kunshu-Z <talwarprth@gmail.com>
2024-09-21 15:23:52 +1200

    Added new Day/Night cycle
    A new day/night cycle has been added which also has a moon rotating during night time. Directional light has been unselected for the time-being as it is currently not required.

---
+++ Assets/Day&Night Cycle/DayNightCycleController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class DayNightCycleController : MonoBehaviour
+{
+	public Transform RotationBase;
+
+	public Light    Sun;
+	public Gradient SunColour;
+
+	public Light    Moon;
+	public Gradient MoonColour;
+
+	public Light          Indoor;
+	public AnimationCurve IndoorIntensity;
+
+	public float speed = 100;
+
+	[Range(0, 24)]
+	public double timeOfDay = 12.0f;
+
+
+	
+	public void Start()
+	{
+	}
+
+
+	public void Update()
+	{
+		double deltaHour = (Time.deltaTime * speed) / 60.0 / 60.0;
+		timeOfDay += deltaHour;
+		while (timeOfDay <   0) { timeOfDay += 24; }
+		while (timeOfDay >= 24) { timeOfDay -= 24; }
+		UpdateElements();
+	}
+
+
+	public void OnValidate()
+	{
+		UpdateElements();
+	}
+
+
+	private void UpdateElements()
+	{
+		float t = (float)(timeOfDay / 24.0);
+
+        if (RotationBase != null)
+        {
+            // rotate celestial bodies (+180: Objects in editor are set up for noon)
+            RotationBase.localRotation = Quaternion.AngleAxis(t * 360.0f + 180.0f, Vector3.right);
+        }
+
+        if (Sun != null)
+        {
+            // set sun/moon colour and intensity
+            Sun.color = SunColour.Evaluate(t);
+            Sun.gameObject.SetActive(Sun.color.maxColorComponent > 0.01f);
+        }
+
+        if (Moon != null)
+        {
+            Moon.color = MoonColour.Evaluate(t);
+            Moon.gameObject.SetActive(Moon.color.maxColorComponent > 0.01f);
+        }
+
+        if (Indoor != null)
+        {
+            // control indoor light
+            Indoor.gameObject.SetActive(IndoorIntensity.Evaluate(t) > 0.5f);
+        }
+
+		// speed change keys
+		if (Input.GetKeyDown(KeyCode.LeftBracket))  speed /= 10.0f;
+		if (Input.GetKeyDown(KeyCode.RightBracket)) speed *= 10.0f;
+	}
+}
