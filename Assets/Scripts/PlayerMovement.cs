f953ba4084e903d95d5d41bb3f9a6c1baa6184ce
Kunshu-Z <talwarprth@gmail.com>
2025-02-22 16:30:55 +1300

    Revert "Conversion to version 6.0 for unity"
    This reverts commit f8b27ddb5e6722d7556104f707bab510de07330d.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,49 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bd172e6949b922d585b455dc2189307dee6b851b4467eee47678087a39a36c11
-size 1753
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed of the capsule movement
+    private Rigidbody rb;
+    public Transform cameraTransform; // Reference to the camera's transform
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from WASD or arrow keys
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a movement vector based on camera's forward direction
+        Vector3 forward = cameraTransform.forward; // Get the camera's forward direction
+        Vector3 right = cameraTransform.right; // Get the camera's right direction
+
+        // Set Y to 0 to ignore vertical movement
+        forward.y = 0;
+        right.y = 0;
+
+        // Normalize vectors to avoid diagonal speed increase
+        forward.Normalize();
+        right.Normalize();
+
+        // Create movement vector relative to the camera's direction
+        Vector3 movement = (forward * moveVertical + right * moveHorizontal).normalized;
+
+        //Applying movement to the Rigidbody
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        //Rotate the capsule to face the movement direction
+        if (movement.magnitude > 0.1)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+    }
+}
